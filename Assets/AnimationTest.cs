7a2b63a67dd2045bc71c7992ecd17e8a5e380f72
Daisuke Suzuki <bboydaisuke@gmail.com>
2024-06-20 12:33:48 +0900

    6/20分の内容を更新した

---
+++ Assets/AnimationTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// あ
+public class AnimationTest : MonoBehaviour
+{
+    Animator _anim;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+            _anim.Play("Idle");
+
+        if (Input.GetKeyDown(KeyCode.B))
+            _anim.Play("Get");
+    }
+
+    void Test01()
+    {
+
+    }
+}
